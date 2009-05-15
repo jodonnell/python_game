2c7ad7947b6978f0c7da8a15bf6e563646285be3
Jacob O'Donnell <jacobodonnell@gmail.com>
2009-05-15 01:10:11 +0000

    cleaned up the collision detection logic

--- sprites/player/moving_left_state.py
+++ sprites/player/moving_left_state.py
@@ -1,13 +1,5 @@
 from pygame.sprite import spritecollide
 
-def collide_left(player, collides, move):
-    left_tiles = [tile for tile in collides if player.rect.left <= tile.rect.right and tile.rect.top < player.rect.bottom]
-    if left_tiles:
-        left_tiles.sort(lambda x,y: x.rect.right - y.rect.right)
-        return move + (left_tiles[0].rect.right - player.rect.left)
-    return move
-
-
 class MovingLeftState():
     "The state when the player is moving left."
     def __init__(self, player):
@@ -25,23 +17,25 @@ class MovingLeftState():
 
     def move_left(self, level):
         "move left"
-        move = -self.player.movement_speed
-        left_end_of_level = level.get_first_tile().get_left_edge()
-        left_edge_of_level_reached = level.view.get_left_end_of_view() + move < left_end_of_level
-
-        move = self.move_left_max(move, level)
+        move = self.get_movement_distance(level)
 
         if move == 0: # trying to move into wall
             self.frame_count = 0
             self.animation_index = 0
             return
         
+        left_end_of_level = level.get_first_tile().get_left_edge()
+        left_edge_of_level_reached = level.view.get_left_end_of_view() + move < left_end_of_level
+
         if (left_edge_of_level_reached or self.player.is_player_right_of_center()):
-            # this will be wrong for movement speeds bigger than 1
             self.player.move_rect_x(move)
         else:
             level.move_view(move)
 
+        self.update_animation()
+
+    def update_animation(self):
+        """Updates the frame count and animation"""
         self.frame_count += 1
         
         if self.frame_count == 8:
@@ -49,18 +43,30 @@ class MovingLeftState():
             self.animation_index += 1
             if self.animation_index == len(self._animation):
                 self.animation_index = 0
-
-    def move_left_max(self, move, level):
+        
+    def get_movement_distance(self, level):
         """Looks at move and sees if player can move that far, if not returns the max amount
         the player is allowed to move"""
-        tmp = self.player.rect
-        self.player.rect = self.player.rect.move(move, 0)
+
+        # move the player left the movement speed and see if he hits a wall
+        move = old_move = -self.player.movement_speed
+
+        self.player.rect.left += move
+
         collides = spritecollide(self.player, level.tile_group, False)
-
         if collides:
-            move = collide_left(self.player, collides, move)
+            move = self._get_movement_until_collision(collides, move)
 
-        self.player.rect = tmp
+        self.player.rect.left -= old_move
+        return move
+
+    def _get_movement_until_collision(self, collides, move):
+        """Given a list of tiles that collide with the player finds the one farthest to the right 
+        and returns the number of pixels the player can move until it is within the tile"""
+        left_collision_tiles = [tile for tile in collides if self.player.rect.left <= tile.rect.right and tile.rect.top < self.player.rect.bottom]
+        if left_collision_tiles:
+            left_collision_tiles.sort(lambda x,y: x.rect.right - y.rect.right) # get the colliding tile furthest to the right
+            move += left_collision_tiles[0].rect.right - self.player.rect.left
         return move
 
     def move_right(self, level):
--- sprites/player/moving_right_state.py
+++ sprites/player/moving_right_state.py
@@ -1,13 +1,5 @@
 from pygame.sprite import spritecollide
 
-def collide_right(player, collides, move):
-    right_tiles = [tile for tile in collides if player.rect.right >= tile.rect.left and tile.rect.top < player.rect.bottom]
-    if right_tiles:
-        right_tiles.sort(lambda x,y: x.rect.left - y.rect.left)
-        return move + (right_tiles[0].rect.left - player.rect.right)
-    return move
-
-
 class MovingRightState():
     "The state when the player is moving right."
     def __init__(self, player):
@@ -25,23 +17,25 @@ class MovingRightState():
 
     def move_right(self, level):
         "move right"
-        move = self.player.movement_speed 
-        right_end_of_level = level.get_last_tile().get_right_edge()
-        right_edge_of_level_reached = level.view.get_right_end_of_view() + move > right_end_of_level
-
-        move = self.move_right_max(move, level)
+        move = self.get_movement_distance(level)
 
         if move == 0: # trying to move into wall
             self.frame_count = 0
             self.animation_index = 0
             return
         
+        right_end_of_level = level.get_last_tile().get_right_edge()
+        right_edge_of_level_reached = level.view.get_right_end_of_view() + move > right_end_of_level
+
         if (right_edge_of_level_reached or self.player.is_player_left_of_center()):
-            # this will be wrong for movement speeds bigger than 1
             self.player.move_rect_x(move)
         else:
             level.move_view(move)
 
+        self.update_animation()
+
+    def update_animation(self):
+        """Updates the frame count and animation"""
         self.frame_count += 1
         
         if self.frame_count == 8:
@@ -50,19 +44,26 @@ class MovingRightState():
             if self.animation_index == len(self._animation):
                 self.animation_index = 0
 
-        return self.player.movement_speed
-
-    def move_right_max(self, move, level):
+    def get_movement_distance(self, level):
         """Looks at move and sees if player can move that far, if not returns the max amount
         the player is allowed to move"""
-        tmp = self.player.rect
-        self.player.rect = self.player.rect.move(move, 0)
+        move = old_move = self.player.movement_speed
+        self.player.rect.left += move
+
         collides = spritecollide(self.player, level.tile_group, False)
-
         if collides:
-            move = collide_right(self.player, collides, move)
+            move = self._get_movement_until_collision(collides, move)
 
-        self.player.rect = tmp
+        self.player.rect.left -= old_move
+        return move
+
+    def _get_movement_until_collision(self, collides, move):
+        """Given a list of tiles that collide with the player finds the one farthest to the left
+        and returns the number of pixels the player can move until it is within the tile"""
+        right_collision_tiles = [tile for tile in collides if self.player.rect.right >= tile.rect.left and tile.rect.top < self.player.rect.bottom]
+        if right_collision_tiles:
+            right_collision_tiles.sort(lambda x,y: x.rect.left - y.rect.left)
+            move += right_collision_tiles[0].rect.left - self.player.rect.right
         return move
 
     def move_left(self, level):
